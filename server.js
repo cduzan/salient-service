65fe463882dd7a86c461a8010ba3d44118be7e54
cduzan <cduzan@themarinergroup.net>
2016-10-11 17:44:17 -0400

    clean up code

--- server.js
+++ server.js
@@ -1,22 +1,77 @@
-var _ = require('lodash');
-var jsonServer = require('json-server')
-var express = require('express');
-var server = jsonServer.create() // Returns an Express server
-var router = jsonServer.router('db.json') // Returns an Express router
-var app = express();
+var natsOptions = {
+    url: "nats://127.0.0.1:4222",
+    reconnect: true,
+    maxReconnectAttempts: 5,
+    reconnectTimeWait: 1000
+};
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
+var nats = require('nats').connect(natsOptions);
+var base64 = require('node-base64-image');
+
+nats.on('error', function(e) {
+    console.log('Error [' + nats.options.url + ']: ' + e);
+    process.exit();
 });
 
-require("./routes/image")(server);
+nats.on('close', function() {
+    console.log('CLOSED');
+    process.exit();
+});
 
-//server.use(jsonServer.defaults) // logger, static and cors middlewares
-//server.use(router) // Mount router on '/'
-//server.listen(5005);
+nats.subscribe('‌getSalientSnapshot', function(request, replyTo) {
+    console.log('Received "' + request + '"');
+    console.log('From "' + replyTo + '"');
 
+    var req = JSON.parse(request);
+
+    getSalientSnapshot(req.endpoint, function(err, results) {
+        console.log('getSalientSnapshot');
+        console.log(results);
+
+        var msg = {
+            imageSrc: 'data:image/jpeg;base64,' + results
+        };
+
+        nats.publish(replyTo, JSON.stringify(msg));
+    });
+});
+
+function getSalientSnapshot(endpoint, callback) {
+
+    /* http://192.168.250.202:8080/cameras/2/media?accept=image/jpeg */
+    var username = "admin";
+    var password = "popb111";
+
+    /*request({
+     url: 'http://' + salientServer + ':8080/cameras/' + cameraId + '/media?accept=image/jpeg',
+     method: 'GET'
+     }, function(error, response, body){
+     if(error) return callback(error);
+     else callback(null, response);
+     });*/
+
+    //var authHeader = basic(username, password);
+
+    /*request
+     .get({
+     url: 'http://' + salientServer + ':8080/cameras/' + cameraId + '/media?accept=image/jpeg',
+     method: 'GET',
+     headers: {
+     'Authorization': authHeader
+     }
+     })
+     .on('error', function(err) {
+     callback(err);
+     })
+     .pipe(res);*/
+
+
+    base64.encode(endpoint, {string: true}, function(err, result) {
+        if (err) {
+            console.log(err);
+            return callback(err);
+        }
+        console.log(result);
+        return callback(err, result);
+    });
+}
